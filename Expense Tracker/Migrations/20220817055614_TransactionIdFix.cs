fdf100210eeac6dc80b2b5807547efd57874e460
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-08-17 12:17:00 +0200

    Add project files.

---
+++ Expense Tracker/Migrations/20220817055614_TransactionIdFix.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class TransactionIdFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TansactionId",
+                table: "Transactions",
+                newName: "TransactionId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "Transactions",
+                newName: "TansactionId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(5)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,33 @@
+using Expense_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// DI
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+// Licensing
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1iX35dcX1WQWBVV0Y=");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.Run();
